74428cabc42340190bfaa5c71dca338f184899f5
NagThota <56493316+NagThota1217@users.noreply.github.com>
2022-12-21 15:33:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,14 +22,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# normalize the jason pay load into a tabular format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the table on the page.
-streamlit.dataframe(fruityvice_normalized)
-#do not run anything past here
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # normalize the jason pay load into a tabular format
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # Display the table on the page.
+      streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+    streamlit.error()
+  #do not run anything past here
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
