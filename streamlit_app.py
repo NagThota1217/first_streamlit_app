24005c8aef21191422ccf200393a800e29fbb2a8
NagThota <56493316+NagThota1217@users.noreply.github.com>
2022-12-21 15:35:07 -0500

    Update streamlit_app.py
    corrected indentattion

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,14 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
+    streamlit.error("Please select a fruit to get information.")
   else:
-      streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      # normalize the jason pay load into a tabular format
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # Display the table on the page.
-      streamlit.dataframe(fruityvice_normalized)
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalize the jason pay load into a tabular format
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Display the table on the page.
+    streamlit.dataframe(fruityvice_normalized)
  except URLError as e:
     streamlit.error()
   #do not run anything past here
